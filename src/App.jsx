0ceb30bf8a759067d7b8c89a23c43869dadc0d10
blaizethelaw <blaizegpt@gmail.com>
2025-08-09 01:15:39 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ServicesPage from "./pages/ServicesPage";
-import AboutPage from "./pages/AboutPage";
-import BookingPage from "./pages/BookingPage";
-import TestimonialsPage from "./pages/TestimonialsPage";
-import ContactPage from "./pages/ContactPage";
-import Navbar from "./components/Navbar";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-import ThreeBackground from "./components/ThreeBackground";
+import React, { useState } from 'react';
+import HomePage from './pages/HomePage';
+import ServicesPage from './pages/ServicesPage';
+import AboutPage from './pages/AboutPage';
+import BookingPage from './pages/BookingPage';
+import TestimonialsPage from './pages/TestimonialsPage';
+import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
+import ScrollToTopButton from './components/ScrollToTopButton';
+import ThreeBackground from './components/ThreeBackground';
+import './index.css';
 
-export default function App() {
+function App() {
+  // We use state to control which page is visible, instead of a router.
+  const [currentPage, setCurrentPage] = useState('home');
+
+  // This function determines which page component to render.
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'services':
+        return <ServicesPage />;
+      case 'about':
+        return <AboutPage />;
+      case 'booking':
+        return <BookingPage />;
+      case 'testimonials':
+        return <TestimonialsPage />;
+      case 'contact':
+        return <ContactPage />;
+      case 'home':
+      default:
+        return <HomePage />;
+    }
+  };
+
   return (
-    <Router>
+    <>
       <ThreeBackground />
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/booking" element={<BookingPage />} />
-          <Route path="/testimonials" element={<TestimonialsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </main>
+      {/* We pass the setCurrentPage function to the Navbar so it can change the active page */}
+      <Navbar setCurrentPage={setCurrentPage} currentPage={currentPage} />
+      <main>{renderPage()}</main>
       <ScrollToTopButton />
-    </Router>
+    </>
   );
 }
+
+export default App;
